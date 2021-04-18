d32475817683ce56323a536f3a2646dd51ced94f
JC <sanjothebay@gmail.com>
2021-04-18 15:55:23 -0500

    deploying tp test out download tesume

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import ContactmePage from "./pages/contactme";
 
 const App = () => {
   return (
-    <Router>
+    <Router  basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/contactme" component={ContactmePage} exact />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/contactme" component={ContactmePage} />
       </Switch>
     </Router>
   );
